dd1a3a6ef9a82a7397abb66d9f694ebc9b5e6e8b
J <jimfinoc@users.noreply.github.com>
2017-11-05 17:55:20 -0500

    CURC code added

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
 import pygame
+pygame.init()
+pygame.joystick.init()
+
+### Tells the number of joysticks/error detection
+joystick_count = pygame.joystick.get_count()
+print "Number of joysticks detected: ", joystick_count
+if joystick_count == 0:
+    print "Error, I did not find any joysticks"
+else:
+    my_joystick=pygame.joystick.Joystick(0)
+    my_joystick.init()
+    axes_count = my_joystick.get_axis
+    print "Number of axes:", axes_count
+    buttons_count = my_joystick.get_numbuttons()
+    print "Number of buttons:", buttons_count
