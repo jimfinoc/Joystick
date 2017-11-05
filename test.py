acb27c53f0f5d7add50b6eb4dc6e431e90abf67d
J <jimfinoc@users.noreply.github.com>
2017-11-05 17:58:36 -0500

    it is axes?

--- test.py
+++ test.py
@@ -11,6 +11,6 @@ else:
     my_joystick=pygame.joystick.Joystick(0)
     my_joystick.init()
     axis_count = my_joystick.get_numaxes()
-    print "Number of axes:", axes_count
+    print "Number of axes:", axis_count
     buttons_count = my_joystick.get_numbuttons()
     print "Number of buttons:", buttons_count
